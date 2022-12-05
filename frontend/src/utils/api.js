f1efcb2cfed601b6054a241b1a9dc456168992f4
Yash <yash.gupta5961@gmail.com>
2022-12-05 14:02:40 -0600

    fixing frontend api request route

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const api = axios.create({
   withCredentials: true,
   credentials: "same-origin",
-  baseURL: "http://localhost:4000/api/v1",
-  // process.env.NODE_ENV === "production"
-  //   ? "https://graderu-api.onrender.com/api/v1"
-  //   : "http://localhost:4000/api/v1",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://graderu.herokuapp.com/api/v1"
+      : "http://localhost:4000/api/v1",
 });
 
 export default api;
