e44337caedca54ffc1b83751ed7a5ecbd36b9faa
Yash <yash.gupta5961@gmail.com>
2022-12-12 14:14:22 -0600

    added routing for prof and course page from home page

--- frontend/src/components/homeScreen/homeScreenProfItem.jsx
+++ frontend/src/components/homeScreen/homeScreenProfItem.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './homeScreenItem.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./homeScreenItem.css";
 
 function HomeScreenSearchItem(props) {
-    return (
-        <div className='homescreen-search-item-container'>
-            <p>{props.name}</p>
-        </div>
-    )
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="homescreen-search-item-container"
+      onClick={() => navigate(`/professors?profName=${props.name}`)}
+    >
+      <p>{props.name}</p>
+    </div>
+  );
 }
 
-export default HomeScreenSearchItem
+export default HomeScreenSearchItem;
--- frontend/src/screens/homeScreen/homeScreen.jsx
+++ frontend/src/screens/homeScreen/homeScreen.jsx
@@ -1,13 +1,13 @@
-import Container from '@mui/material/Container';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import React, { useState } from 'react';
-import HomeScreenProfItem from '../../components/homeScreen/homeScreenProfItem';
-import HomeScreenCourseItem from '../../components/homeScreen/homeScreenCourseItem';
-import './homeScreen.css';
+import Container from "@mui/material/Container";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import React, { useState } from "react";
+import HomeScreenProfItem from "../../components/homeScreen/homeScreenProfItem";
+import HomeScreenCourseItem from "../../components/homeScreen/homeScreenCourseItem";
+import "./homeScreen.css";
 
 // const data = [
 //     { name: "Belgium", continent: "Europe" },
@@ -37,168 +37,178 @@ import './homeScreen.css';
 // ];
 
 function HomeScreen() {
-    const [data, setData] = useState({ data: [] });
-    const [searchInput, setSearchInput] = useState("");
-    const [filter, setFilter] = React.useState('Professor');
-    const [isLoading, setIsLoading] = useState(false);
-    const [err, setErr] = useState('');
+  const [data, setData] = useState({ data: [] });
+  const [searchInput, setSearchInput] = useState("");
+  const [filter, setFilter] = React.useState("Professor");
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState("");
 
-    const handleChangeSearch = (event) => {
-        event.preventDefault();
-        setSearchInput(event.target.value);
+  const handleChangeSearch = (event) => {
+    event.preventDefault();
+    setSearchInput(event.target.value);
+  };
+
+  const handleProfAPICall = async (name) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(
+        "https://graderu.herokuapp.com/api/v1/professors?name=" + name,
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    const handleProfAPICall = async (name) => {
-        setIsLoading(true);
-        try {
-            const response = await fetch('https://graderu.herokuapp.com/api/v1/professors?name=' + name, {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                },
-            });
-
-            if (!response.ok) {
-                throw new Error(`Error! status: ${response.status}`);
-            }
-
-            const result = await response.json();
-
-            setData(result);
-        } catch (err) {
-            setErr(err.message);
-        } finally {
-            setIsLoading(false);
+  const handleCourseAPICall = async (input) => {
+    setIsLoading(true);
+    try {
+      const words = input.split(" ");
+      const response = await fetch(
+        "https://graderu.herokuapp.com/api/v1/courses?subject=" +
+          words[0] +
+          "&number=" +
+          words[1],
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
         }
-    };
+      );
 
-    const handleCourseAPICall = async (input) => {
-        setIsLoading(true);
-        try {
-            const words = input.split(' ');
-            const response = await fetch('https://graderu.herokuapp.com/api/v1/courses?subject=' + words[0] + '&number=' + words[1], {
-                method: 'GET',
-                headers: {
-                    Accept: 'application/json',
-                },
-            });
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
 
-            if (!response.ok) {
-                throw new Error(`Error! status: ${response.status}`);
-            }
+      const result = await response.json();
 
-            const result = await response.json();
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-            setData(result);
-        } catch (err) {
-            setErr(err.message);
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter" && event.target.value !== "") {
+      if (filter === "Professor") {
+        setSearchInput(event.target.value);
+        handleProfAPICall(event.target.value);
+        console.log("Professor Search");
+      } else if (filter === "Course") {
+        setSearchInput(event.target.value);
+        handleCourseAPICall(event.target.value);
+        console.log("Course Search");
+      }
+    }
+  };
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter' && event.target.value !== '') {
-            if (filter === "Professor") {
-                setSearchInput(event.target.value);
-                handleProfAPICall(event.target.value);
-                console.log("Professor Search");
-            }
-            else if (filter === "Course") {
-                setSearchInput(event.target.value);
-                handleCourseAPICall(event.target.value);
-                console.log("Course Search");
-            }
-        }
-    };
+  const handleChangeFilter = (event) => {
+    setFilter(event.target.value);
+    setData({ data: [] });
+  };
 
-    const handleChangeFilter = (event) => {
-        setFilter(event.target.value);
-        setData({ data: [] });
-    };
+  return (
+    <Container
+      className="homescreen-container"
+      sx={{
+        backgroundColor: "primary.background",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div className="homescreen-search">
+        <TextField
+          id="search-bar"
+          className="homescreen-searchbox"
+          variant="outlined"
+          placeholder={
+            filter === "Professor" ? "Caesar, Matthew..." : "CS 374..."
+          }
+          onChange={handleChangeSearch}
+          onKeyDown={handleKeyDown}
+        />
 
-    return (
-        <Container className="homescreen-container"
-            sx={{
-                backgroundColor: "primary.background",
-                display: "flex",
-                flexDirection: "column",
-            }}
+        <div className="homescreen-search-params">
+          <FormControl>
+            <InputLabel id="home-search-filter-label">Search Filter</InputLabel>
+            <Select
+              labelId="home-search-filter-label"
+              id="demo-simple-select"
+              value={filter}
+              label="Search Filter"
+              className="homescreen-filters"
+              onChange={handleChangeFilter}
+            >
+              <MenuItem value={"Professor"}>Professor</MenuItem>
+              <MenuItem value={"Course"}>Course</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+
+      {filter === "Professor" ? (
+        <div
+          className="homescreen-results"
+          style={
+            data.data.length > 0
+              ? {
+                  border: "3px solid black",
+                  boxShadow: "5px 5px",
+                  overflowY: "scroll",
+                }
+              : {}
+          }
         >
-
-            <div className="homescreen-search">
-                <TextField
-                    id="search-bar"
-                    className="homescreen-searchbox"
-                    variant="outlined"
-                    placeholder={
-                        (filter === "Professor") ? "Caesar, Matthew..." : "CS 374..."
-                    }
-                    onChange={handleChangeSearch}
-                    onKeyDown={handleKeyDown}
-                />
-
-                <div className="homescreen-search-params">
-                    <FormControl>
-                        <InputLabel id="home-search-filter-label">Search Filter</InputLabel>
-                        <Select
-                            labelId="home-search-filter-label"
-                            id="demo-simple-select"
-                            value={filter}
-                            label="Search Filter"
-                            className='homescreen-filters'
-                            onChange={handleChangeFilter}
-                        >
-                            <MenuItem value={"Professor"}>Professor</MenuItem>
-                            <MenuItem value={"Course"}>Course</MenuItem>
-                        </Select>
-                    </FormControl>
-                </div>
-            </div>
-
-            {(filter === "Professor") ? (
-                <div className="homescreen-results"
-                    style={
-                        data.data.length > 0 ? {
-                            border: "3px solid black",
-                            boxShadow: "5px 5px",
-                            overflowY: "scroll"
-                        } : {}
-                    }
-                >
-                    <ul className="homescreen-results-list">
-                        {
-                            data.data.map((item) => (
-                                <li key={item._id} >
-                                    <HomeScreenProfItem name={item.name} />
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            ) : (
-                <div className="homescreen-results"
-                    style={
-                        data.data.length > 0 ? {
-                            border: "3px solid black",
-                            boxShadow: "5px 5px",
-                            overflowY: "scroll"
-                        } : {}
-                    }
-                >
-                    <ul className="homescreen-results-list">
-                        {
-                            data.data.map((item) => (
-                                <li key={item._id} >
-                                    <HomeScreenCourseItem data={item} />
-                                </li>
-                            ))
-                        }
-                    </ul>
-                </div>
-            )}
-        </Container>
-    )
+          <ul className="homescreen-results-list">
+            {data.data.map((item) => (
+              <li key={item._id}>
+                <HomeScreenProfItem name={item.name} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div
+          className="homescreen-results"
+          style={
+            data.data.length > 0
+              ? {
+                  border: "3px solid black",
+                  boxShadow: "5px 5px",
+                  overflowY: "scroll",
+                }
+              : {}
+          }
+        >
+          <ul className="homescreen-results-list">
+            {data.data.map((item) => (
+              <li key={item._id}>
+                <HomeScreenCourseItem data={item} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </Container>
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
